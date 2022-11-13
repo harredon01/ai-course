1f21c434a063a546b67ca52dbadbba29470e9cde
Hoovert Arredondo <hoovertarredondo@Hooverts-MBP.lan>
2022-11-13 12:18:44 -0800

    Brute forcing for params

--- hw3/NeuralNetwork3.py
+++ hw3/NeuralNetwork3.py
@@ -1,5 +1,7 @@
 import sys
 import numpy as np
+import math
+import time
 arguments = sys.argv
 train_data = arguments[1]
 train_labels = arguments[2]
@@ -51,34 +53,25 @@ def predict( vals):
         result.append(output)
     return result
 
-def load_data(train_data_file,train_label_file,test_data,test_labels):
-    with open(train_data_file,'r') as i:
-        lines = i.readlines()
-    with open(train_label_file,'r') as i:
-        lines2 = i.readlines()
-    with open(test_data,'r') as i:
-        lines3 = i.readlines()
-    with open(test_labels,'r') as i:
-        lines4 = i.readlines()
+def load_data(lines,lines2):
     tr_arr = []
     tr_lbs_arr = []
     for it in range(len(lines)):
         tup = lines[it].replace("\n","").split(",")
-        tr_arr.append([[float(tup[0]),float(tup[1])]])
-        tr_lbs_arr.append([[float(lines2[it].replace("\n",""))]])
+        #tr_arr.append([[float(tup[0]),float(tup[1])]])
+        try:
+            tr_arr.append([[float(tup[0]),float(tup[1])]])
+            tr_lbs_arr.append([[float(lines2[it].replace("\n",""))]])
+        except:
+            print("ERROR")
+            print(tup)
+            print(lines2[it])
+            exit()
     x_train = np.array(tr_arr)
     y_train = np.array(tr_lbs_arr)
-    tr_arr = []
-    tr_lbs_arr = []
-    for it in range(len(lines3)):
-        tup = lines3[it].replace("\n","").split(",")
-        tr_arr.append([[float(tup[0]),float(tup[1])]])
-        tr_lbs_arr.append([[float(lines4[it].replace("\n",""))]])
-    x_test = np.array(tr_arr)
-    y_test = np.array(tr_lbs_arr)
-    return x_train,y_train,x_test,y_test
+    return x_train,y_train
 
-def train( x_train, y_train, epochs, alpha):
+def train( x_train, y_train, alpha, epochs):
     data_s = len(x_train)
     for i in range(epochs):
         tot_err = 0
@@ -87,18 +80,19 @@ def train( x_train, y_train, epochs, alpha):
             for level in levels:
                 output = level.f_prop(output)
             tot_err += np.mean(np.power(y_train[j]-output, 2))
+
             error = 2*(output-y_train[j])/y_train[j].size
             for level in reversed(levels):
                 error = level.b_prop(error, alpha)
         tot_err /= data_s
-        print('epoch %d/%d   error=%f' % (i+1, epochs, tot_err))
+        #print('epoch %d/%d   error=%f' % (i+1, epochs, tot_err))
 
 # training data
 x_train = np.array([[[0,0]], [[0,1]], [[1,0]], [[1,1]]])
 y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
 #x_train, y_train = load_data(train_data,train_labels)
 # network
-def build_levels(z,x,y,w):
+def build_levels(z,x,y,w,o,p):
     global levels
     levels = []
     if z == 1:
@@ -107,33 +101,132 @@ def build_levels(z,x,y,w):
         levels.append(FClevel(2, x))
         levels.append(FClevel(x, 1))
     elif z ==3:
-        levels.append(FClevel(3, x))
+        levels.append(FClevel(2, x))
         levels.append(FClevel(x, y))
         levels.append(FClevel(y, 1))
-    elif z == 3:
-        levels.append(FClevel(3, w))
-        levels.append(FClevel(w, x))
+    elif z == 4:
+        levels.append(FClevel(2, x))
         levels.append(FClevel(x, y))
-        levels.append(FClevel(y, 1))
-        
-def run_model():
-    for it in files:
-        train_file = it+"_train_data.csv"
-        train_labels = it+"_train_label.csv"
-        test_labels = it+"_test_data.csv"
-        test_data = it+"_test_label.csv"
-        x_train, y_train = load_data(train_file,train_labels,test_data,test_labels)
-        build_levels(1,0,0,0)
-        train(x_train, y_train, epochs=10, alpha=0.1)
-        out = predict(x_train)
-        for i in range(4):
+        levels.append(FClevel(y, w))
+        levels.append(FClevel(w, 1))
+    elif z == 5:
+        levels.append(FClevel(2, x))
+        levels.append(FClevel(x, y))
+        levels.append(FClevel(y, w))
+        levels.append(FClevel(w, o))
+        levels.append(FClevel(o, 1))
+    elif z == 6:
+        levels.append(FClevel(2, x))
+        levels.append(FClevel(x, y))
+        levels.append(FClevel(y, w))
+        levels.append(FClevel(w, o))
+        levels.append(FClevel(o, p))
+        levels.append(FClevel(p, 1))
 
-            for j in range(4):
-                for k in range(4):
-                    for l in range(4):
-                        build_levels()
+def get_error(predictions,real):
+    tot_err = 0
+    for it in range(len(predictions)):
+        tot_err += np.mean(np.power(real[it]-predictions[it], 2))
+    return tot_err
 
+def bla(z,x,y,w,o,p,lines1,lines2,lines3,lines4,batches,alpha):
+    x_test,y_test = load_data(lines3,lines4)
+    build_levels(z,x,y,w,o,p)
+    bat_size = math.floor(len(lines1)/batches)
+    counter = 0
+    start_time = time.time()
+    for item in range(batches):
+        end = counter + bat_size
+        x_train, y_train = load_data(lines1[counter:end],lines2[counter:end])
+        train(x_train, y_train, alpha, epochs=10)
+        counter = counter + bat_size
+    train_time = time.time()-start_time
+    x_test,y_test = load_data(lines3,lines4)
+    out = predict(x_test)
+    error = get_error(out,y_test)
+    error = str(error)+","+str(train_time)
+    return error
 
+def run_model(lines1,lines2,lines3,lines4,it,batches,alpha):
+    results = {}
+    key = it+"-"+str(1)+"-"+str(0)+"-"+str(0)+"-"+str(0)+"-"+str(0)+"-"+str(0)+"-"+str(batches)+"-"+str(alpha)
+    error = bla(1,0,0,0,0,0,lines1,lines2,lines3,lines4,batches,alpha)
+    print("With: ",key," error: ",error)
+    results[key]=error
+    for i in range(1,4):
+        key = it+"-"+str(2)+"-"+str(i)+"-"+str(0)+"-"+str(0)+"-"+str(0)+"-"+str(0)+"-"+str(batches)+"-"+str(alpha)
+        error = bla(2,i,0,0,0,0,lines1,lines2,lines3,lines4,batches,alpha)
+        print("With: ",key," error: ",error)
+        results[key]=error
+        for j in range(1,4):
+            key = it+"-"+str(3)+"-"+str(i)+"-"+str(j)+"-"+str(0)+"-"+str(0)+"-"+str(0)+"-"+str(batches)+"-"+str(alpha)
+            error = bla(3,i,j,0,0,0,lines1,lines2,lines3,lines4,batches,alpha)
+            print("With: ",key," error: ",error)
+            results[key]=error
+            for k in range(1,4):
+                key = it+"-"+str(4)+"-"+str(i)+"-"+str(j)+"-"+str(k)+"-"+str(0)+"-"+str(0)+"-"+str(batches)+"-"+str(alpha)
+                error = bla(4,i,j,k,0,0,lines1,lines2,lines3,lines4,batches,alpha)
+                print("With: ",key," error: ",error)
+                results[key]=error
+                for o in range(1,4):
+                    key = it+"-"+str(4)+"-"+str(i)+"-"+str(j)+"-"+str(k)+"-"+str(o)+"-"+str(0)+"-"+str(batches)+"-"+str(alpha)
+                    error = bla(5,i,j,k,o,0,lines1,lines2,lines3,lines4,batches,alpha)
+                    print("With: ",key," error: ",error)
+                    results[key]=error
+                    for p in range(1,4):
+                        key = it+"-"+str(4)+"-"+str(i)+"-"+str(j)+"-"+str(k)+"-"+str(o)+"-"+str(p)+"-"+str(batches)+"-"+str(alpha)
+                        error = bla(6,i,j,k,o,p,lines1,lines2,lines3,lines4,batches,alpha)
+                        print("With: ",key," error: ",error)
+                        results[key]=error
+    text_file = open("results.csv", "a")
+    for it in results:
+        val = str(it)+","+str(results[it])+"\n"
+        text_file.write(val)
+    text_file.close()
+
+for it in files:
+    train_file = it+"_train_data.csv"
+    train_labels = it+"_train_label.csv"
+    test_data = it+"_test_data.csv"
+    test_labels = it+"_test_label.csv"
+    with open(train_file,'r') as i:
+        lines1 = i.readlines()
+    with open(train_labels,'r') as i:
+        lines2 = i.readlines()
+    with open(test_data,'r') as i:
+        lines3 = i.readlines()
+    with open(test_labels,'r') as i:
+        lines4 = i.readlines()
+
+    run_model(lines1,lines2,lines3,lines4,it,10,0.1)
+    run_model(lines1,lines2,lines3,lines4,it,10,0.01)
+    run_model(lines1,lines2,lines3,lines4,it,10,0.001)
+    run_model(lines1,lines2,lines3,lines4,it,10,0.2)
+    run_model(lines1,lines2,lines3,lines4,it,10,0.02)
+    run_model(lines1,lines2,lines3,lines4,it,10,0.002)
+    run_model(lines1,lines2,lines3,lines4,it,10,0.3)
+    run_model(lines1,lines2,lines3,lines4,it,10,0.03)
+    run_model(lines1,lines2,lines3,lines4,it,10,0.003)
+
+exit()
+
+with open(train_data,'r') as i:
+    lines1 = i.readlines()
+with open(train_labels,'r') as i:
+    lines2 = i.readlines()
+with open(test_data,'r') as i:
+    lines3 = i.readlines()
+alpha=0.1
+build_levels(z,x,y,x)
+bat_size = math.floor(len(lines1)/batches)
+counter = 0
+for item in range(batches):
+    end = counter + bat_size
+    x_train, y_train = load_data(lines1[counter:end],lines2[counter:end])
+    train(x_train, y_train, alpha, epochs=10)
+    counter = counter + bat_size
+x_test,y_test = load_data(lines3,lines3)
+out = predict(x_test)
 text_file = open("test_predictions.csv", "w")
 for it in out:
     val = str(it[0][0])+"\n"
